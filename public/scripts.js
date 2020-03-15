885608e1a1e24a623a9c601bc6490cb0d3b17d51
Aydan Pirani <aydanpirani@gmail.com>
2020-03-15 11:24:45 -0700

    Added Firebase integration

--- public/scripts.js
+++ public/scripts.js
@@ -1,57 +1,36 @@
-var firebase = require("firebase/app");
-require("firebase/auth");
 
-var firebaseConfig = {
-    apiKey: "AIzaSyAo-Dxsng50G-H71fn6umwIieUZkG87_M0",
-    authDomain: "supplies2seniors-26e2f.firebaseapp.com",
-    databaseURL: "https://supplies2seniors-26e2f.firebaseio.com",
-    projectId: "supplies2seniors-26e2f",
-    storageBucket: "supplies2seniors-26e2f.appspot.com",
-    messagingSenderId: "1074662729499",
-    appId: "1:1074662729499:web:b6c7226d42202777a355be",
-    measurementId: "G-3H6ZNG3VB5"
-  };
-  firebase.initializeApp(config);
+//TEST USER SIGNED-IN?
+var user = firebase.auth().currentUser;
 
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        //Transfer to next HTML webpage
-    } else {
-        // No user is signed in.
-    }
-});
-
-function create() {
-    window.alert("Creating acc!");
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        window.alert("inside");
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        
-        window.alert("Error: " + errorMessage);
-        // ...
-      });      
+if (user) {
+    // User is signed in.
+} else {
+    // No user is signed in.
 }
-
 function login() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    window.alert("Inside login");
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-        window.alert("Inside sign-in");
+        // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        window.alert("Error: " + errorMessage);
+        window.alert("FAILED: " + errorMessage);
+
         // ...
     });
-    window.alert("Outside sign-in");
 }
 
-function logout() {
-    firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-      }).catch(function(error) {
-        // An error happened.
-      });      
+function create() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        window.alert("FAILED: " + errorMessage);
+
+        // ...
+    });
+    window.alert("CREATED.");
 }
